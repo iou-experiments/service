3bccf5083053f342b348c90d61896c59f4b75160
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-07-10 10:47:09 -0400

    clean up

--- server/src/routes/notes.rs
+++ server/src/routes/notes.rs
@@ -1,9 +1,8 @@
-use crate::crypto::note::Note;
-
 pub async fn get_note() -> String {
   "got note!".to_owned()
 }
 
-pub async fn save_note() -> String {
-  "saved note!".to_owned()
+pub async fn save_note(note: String) -> Result<String, String> {
+  println!("Note saved! Serialized data: {}", note); // Print a message with the serialized data
+  Ok("serizlised".to_owned())
 }
